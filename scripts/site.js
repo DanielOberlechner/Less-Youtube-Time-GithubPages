f99a43275630f55dbaa97f70ed24afc2f075772a
Daniel Oberlechner <danieloberlechner@hotmail.de>
2019-01-03 21:19:08 +0100

    New Version of Website

--- scripts/site.js
+++ scripts/site.js
@@ -9,3 +9,25 @@ $(document).ready(function() {
         $(window).scrollTo($('#contact'), 3000);
     })
 })
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     window.onscroll = function() {
+//         console.log("we're scrolling");
+//         //var scroller = document.getElementById('body');
+//         var scroller = window;
+//         var sh = scroller.offsetHeight;
+//         var indicator = document.getElementById('indicator');
+//         var ww = document.documentElement.offsetWidth;
+//         var wh = document.documentElement.offsetHeight;
+//         var y = window.pageYOffset + wh;
+//         var idHeight = (wh - (((sh - wh)- (y - wh)) * (wh / (sh - wh))));
+//         indicator.style.height = idHeight + "px";
+//         if(y >= sh) {
+//             console.log("Set go CRAZY")
+//             document.body.setAttribute('class', 'gocrazy');
+//         } else {
+//             console.log("Remove GOCrazy...")
+//             document.body.removeAttribute('class');
+//         }
+//     }
+//   }, false);

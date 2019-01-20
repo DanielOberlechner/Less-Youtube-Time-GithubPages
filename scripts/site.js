d90372fb71559f412b1e50b8462dd605da749bb9
Daniel Oberlechner <danieloberlechner@hotmail.de>
2019-01-20 23:08:21 +0100

    adding the new merchandise option which is per default enabled ...

--- scripts/site.js
+++ scripts/site.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    document.getElementById('link2').addEventListener('click', function(){
+    document.getElementById('link2').addEventListener('click', function() {
         $(window).scrollTo($('#main'), 500);
     })
-    document.getElementById('link3').addEventListener('click', function(){
+    document.getElementById('link3').addEventListener('click', function() {
         $(window).scrollTo($('#functions'), 1000);
     })
-    document.getElementById('link4').addEventListener('click', function(){
+    document.getElementById('link4').addEventListener('click', function() {
         $(window).scrollTo($('#contact'), 1500);
     })
 })
